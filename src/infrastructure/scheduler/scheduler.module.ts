a73671bd41259d490552c154bdba5a2c937b432c
96Jerry <junmate12@gmail.com>
2026-05-10 04:38:46 +0900

    fix: SchedulerModule에 ProfilerModule 의존성 추가

--- src/infrastructure/scheduler/scheduler.module.ts
+++ src/infrastructure/scheduler/scheduler.module.ts
@@ -7,9 +7,16 @@ import { DiscordModule } from '../discord/discord.module.js';
 import { DailyMealPlanJob } from './jobs/daily-meal-plan.job.js';
 import { DailyMotivationJob } from './jobs/daily-motivation.job.js';
 import { EveningCollectionJob } from './jobs/evening-collection.job.js';
+import { ProfilerModule } from '@infra/profiler/profiler.module.js';
 
 @Module({
-  imports: [MealPlanModule, UserModule, DiscordModule, MotivationModule],
+  imports: [
+    MealPlanModule,
+    UserModule,
+    DiscordModule,
+    MotivationModule,
+    ProfilerModule,
+  ],
   providers: [DailyMealPlanJob, DailyMotivationJob, EveningCollectionJob],
 })
 export class SchedulerModule {}
